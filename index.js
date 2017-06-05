018b0e842b0f85f81a42edd7fc4297d38aea9663
Kumar <shivankumar@lm-sjn-21010683.paypalcorp.com>
2017-06-05 14:20:23 -0700

    working with actions-on-google npm

--- index.js
+++ index.js
@@ -2,56 +2,70 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
 
 
 const restService = express();
 restService.use(bodyParser.json());
 
-restService.post('/hook', function (req, res) {
+restService.post('/hook', function(req, res) {
+  console.log('hook request');
+  console.log("req.body: ");
+  console.log(req.body);
 
-    console.log('hook request');
-    console.log("req.body: ");
-    console.log(req.body);
+  const app = new ApiAiApp({
+    req: req,
+    res: res
+  });
 
-    try {
-        var speech = 'empty speech';
+  function anything(app){
+    console.log("In anything function");
+    console.log(app);
+  }
 
-        if (req.body) {
-            var requestBody = req.body;
+  const actionMap = new Map();
+  actionMap.set('anything', anything);
+  app.handleRequest(actionMap);
 
-            if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }
+  // try {
+  //   var speech = 'empty speech';
+  //
+  //   if (req.body) {
+  //     var requestBody = req.body;
+  //
+  //     if (requestBody.result) {
+  //       speech = '';
+  //
+  //       if (requestBody.result.fulfillment) {
+  //         speech += requestBody.result.fulfillment.speech;
+  //         speech += ' ';
+  //       }
+  //
+  //       if (requestBody.result.action) {
+  //         speech += 'action: ' + requestBody.result.action;
+  //       }
+  //     }
+  //   }
+  //
+  //   console.log('result: ', speech);
+  //
+  //   return res.json({
+  //     speech: speech,
+  //     displayText: speech,
+  //     source: 'apiai-webhook-sample'
+  //   });
+  // } catch (err) {
+  //   console.error("Can't process request", err);
+  //
+  //   return res.status(400).json({
+  //     status: {
+  //       code: 400,
+  //       errorType: err.message
+  //     }
+  //   });
+  // }
 });
 
-restService.listen((process.env.PORT || 5000), function () {
-    console.log("Server listening");
+restService.listen((process.env.PORT || 5000), function() {
+  console.log("Server listening");
 });
